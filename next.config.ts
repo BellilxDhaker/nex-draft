ad398e8300f89a20635d8e4a78c3067e2f1617fb
Dhaker Belil <bellilxdhaker@gmail.com>
2026-05-20 18:06:15 +0200

    Update Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.vectorlogo.zone",
+      },
+      {
+        protocol: "https",
+        hostname: "www.supabase.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
