67374504a88f13861120e35886e2278ba942f1c7
dhaker belil <bellilxdhaker@gmail.com>
2026-05-23 00:21:18 +0200

    refactor: adjust section spacing and reorder page sections

--- app/page.tsx
+++ app/page.tsx
@@ -23,8 +23,8 @@ export default function Home() {
       <Navbar />
       <Hero />
       <SocialProof />
-      <HowItWorks />
       <DocumentationSuite />
+      <HowItWorks />
       <FeatureShowcase />
       <DashboardPreview />
       <Pricing />
