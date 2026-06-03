67644bd7982b957d359f576e451e84c019f4fa80
dhaker belil <bellilxdhaker@gmail.com>
2026-06-03 15:19:11 +0200

    chore: update next config

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,27 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "www.supabase.com",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh4.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh5.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh6.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "*.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
     ],
   },
 };
